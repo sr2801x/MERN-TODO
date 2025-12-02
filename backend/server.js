c140c9da3c062bbbfe7799dedcd3e25448a36c18
rathoresneha <rathoresneha2801@gmail.com>
2025-12-02 20:13:26 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,37 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import cors from 'cors'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log("DB connected"))
-  .catch((err) => console.log("DB connection error:", err));
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> console.log("DB connected"))
+.catch(()=> console.log(err))
 
-const Todo = mongoose.model('Todo', new mongoose.Schema({
-  text: String
+const Todo = mongoose.model("Todo", new mongoose.Schema({
+    text: String
 }));
 
-app.get("/todos", async (req, res) => {
-  const todos = await Todo.find();
-  res.json(todos);
-});
+app.get("/todos", async (req,res) => {
+    res.json(await Todo.find());
+})
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server started on port ${process.env.PORT}`);
-});
+app.post('/todos', async (req,res) => {
+    const todo = await Todo.create({ text : req.body.text });
+    res.json(todo);
+})
+
+app.delete("/todo/:id", async (req, res) => {
+    await Todo.findByIdAndDelete(req.params.id);
+    res.json({message : "Deleted"})
+})
+
+app.listen(process.env.PORT, () =>
+    console.log("Server running on port", process.env.PORT)
+)
